ad5e5da1bfc2fa2a2b4d0c4a8fe4b9aa45a2003d
Devendrajaiswal <109809511+DevendraJaiswal074@users.noreply.github.com>
2024-06-14 13:10:39 +0530

    Add files via upload

---
+++ pattern10.java
@@ -0,0 +1,55 @@
+/* Print the following pattern
+
+*      *
+**    **
+***  ***
+********
+********
+***  ***
+**    **
+*      *
+
+ */
+
+public class pattern10 {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // int space = 2*(n-1);
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // int space = 2*(n-1);
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
