ceb12df70f224ccde4d08d331b8002d31530666d
Devendrajaiswal <109809511+DevendraJaiswal074@users.noreply.github.com>
2024-06-21 23:50:01 +0530

    Add files via upload

---
+++ Hackerrank_substring.java
@@ -0,0 +1,26 @@
+// The first line contains a single string denoting s.
+// The second line contains two space-separated integers denoting the respective values of start and end
+//Sample Input
+// Helloworld
+// 3 7
+// Sample Output
+// lowo
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Hackerrank_substring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+
+        in.close();
+    }
+}
