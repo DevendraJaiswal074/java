a096bd63229b08a3c2e5a64a6351993f381bbad1
Devendrajaiswal <109809511+DevendraJaiswal074@users.noreply.github.com>
2024-06-22 22:21:01 +0530

    Add files via upload

---
+++ Recursion_str_reverse.java
@@ -0,0 +1,20 @@
+//print the string in reverse order using recursion
+
+import java.util.*;
+public class Recursion_str_reverse {
+    public static void print_str_reverse(String user,int index) {
+        if (index == 0) {                         // Base case
+            System.out.print(user.charAt(0));      // print the last value of reverse
+            return;                          // exit the function
+        }
+        System.out.print(user.charAt(index));
+        print_str_reverse(user, index-1);           
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String user = scan.next();
+        print_str_reverse(user, user.length()-1);
+        scan.close();
+    }
+}
