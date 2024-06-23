5055b8bb405de4f9a05911d334066d312c7cc426
Devendrajaiswal <109809511+DevendraJaiswal074@users.noreply.github.com>
2024-06-23 15:27:47 +0530

    Add files via upload

---
+++ Recursion_str_Subsequence.java
@@ -0,0 +1,32 @@
+// print all the subsequence of a string "dev"
+/*OUTPUT
+dev
+de
+dv
+d
+ev
+e
+v
+
+ */
+
+public class Recursion_str_Subsequence {
+
+    public static void subsequence(String str, int index, String new_str) {
+        if (index == str.length()) {
+            System.out.println(new_str);
+            return;
+        }
+        char current_char = str.charAt(index);
+
+        subsequence(str, index + 1, new_str + current_char);
+        subsequence(str, index + 1, new_str);
+
+    }
+
+    public static void main(String[] args) {
+        String str = "dev";
+        subsequence(str, 0, "");
+    }
+
+}
