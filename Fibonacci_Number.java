4847c3641cbb30a2a980f4f77de0f3531c805f4a
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-07-05 21:50:45 +0530

    Coding Ninjas

---
+++ Fibonacci_Number.java
@@ -0,0 +1,46 @@
+// https://www.naukri.com/code360/problems/nth-fibonacci-number_74156?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+/* 
+
+Problem statement
+Send feedback
+The n-th term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+
+    F(n) = F(n - 1) + F(n - 2), 
+    Where, F(1) = 1, F(2) = 1
+
+
+Provided 'n' you have to find out the n-th Fibonacci Number. Handle edges cases like when 'n' = 1 or 'n' = 2 by using conditionals like if else and return what's expected.
+
+"Indexing is start from 1"
+
+*/
+import java.util.*;
+public class Fibonacci_Number {
+	public static int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            int a = 1, b = 1, fib = 1;
+            for (int i = 3; i <= n; i++) {
+                fib = a + b;
+                a = b;
+                b = fib;
+            }
+            return fib;
+        }
+    }
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+		Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(fibonacci(n));
+	}
+
+}
