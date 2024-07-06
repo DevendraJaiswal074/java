d7e9c57353306e8f046bcadd28b4d8c67bd7136a
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-07-06 21:25:58 +0530

    CodingNinjas(pattern_number)

---
+++ pattern_Right_triangle_number.java
@@ -0,0 +1,31 @@
+/*
+https://www.naukri.com/code360/problems/n-triangles_6573689?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=PROBLEM
+
+ Problem statement
+Send feedback
+Sam is making a Triangular painting for a maths project.
+
+An N-dimensional Triangle is represented by the lower triangle of the pattern filled with integers starting from 1.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+1
+1 2 
+1 2 3
+ */
+
+ public class pattern_Right_triangle_number {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(j+1+" ");
+            }
+            System.out.println();
+        }
+    }
+}
