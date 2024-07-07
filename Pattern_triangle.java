252ddd730e4f352d4c9163a98c1263f1dfd5ecdf
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-07-07 12:44:18 +0530

    CodingNinjas(Pattern)

---
+++ Pattern_triangle.java
@@ -0,0 +1,33 @@
+/*
+https://www.naukri.com/code360/problems/star-triangle_6573671?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+
+Problem statement
+Send feedback
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+ */
+
+public class Pattern_triangle {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
