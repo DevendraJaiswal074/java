11380dfa58aee6164c4f402ae9db88a971623187
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-07-08 13:59:22 +0530

    Hackerrank(ProjectEuler#1)

---
+++ Project_Euler_1.java
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler001/problem?isFullScreen=true
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Project_Euler_1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int sum = 0;
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            for (int i = 1; i < n; i++) {
+                if (i%3==0 || i%5==0) {
+                    sum = sum+i;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
