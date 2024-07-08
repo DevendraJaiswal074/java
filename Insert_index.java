b66e6a6060920ec66d22239965599255cc6053c5
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-07-08 11:32:03 +0530

    leetCode(35)

---
+++ Insert_index.java
@@ -0,0 +1,43 @@
+/*
+
+https://leetcode.com/problems/search-insert-position/
+
+ Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+ */
+
+public class Insert_index {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,6};
+        int target = 5;
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
