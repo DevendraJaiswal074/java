9b27e952f677c1463904e4642014bada631af66e
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-10-11 00:34:23 +0530

    LeetCode

--- Happy_Number.java
+++ Happy_Number.java
@@ -37,3 +37,20 @@ class Happy_Number {
            
        }
    }
+
+/*
+ Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+Example 2:
+
+Input: n = 2
+Output: false
+ */
