81d3cf4d7a0a8a8d31c6b4a680c3548436653aa1
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-10-31 20:43:36 +0530

    LeetCode

---
+++ 1732. Find the Highest Altitude.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/find-the-highest-altitude/description/
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+      int ans = 0;
+      int currAltitude = 0;
+      for (final int g : gain) {
+        currAltitude += g;
+        ans = Math.max(ans, currAltitude);
+      }
+      return ans;
+    }
+  }
