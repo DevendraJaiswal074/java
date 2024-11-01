c712d31140d3f53ae6e7cd705172f979a2082f90
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2024-11-01 19:51:36 +0530

    LeetCode

---
+++ 071. Greatest Common Divisor of Strings.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+      if (str1.length() < str2.length())
+        return gcdOfStrings(str2, str1);
+      if (!str1.startsWith(str2))
+        return "";
+      if (str2.isEmpty())
+        return str1;
+      return gcdOfStrings(str2, mod(str1, str2));
+    }
+  
+    private String mod(String s1, final String s2) {
+      while (s1.startsWith(s2))
+        s1 = s1.substring(s2.length());
+      return s1;
+    }
+  }
