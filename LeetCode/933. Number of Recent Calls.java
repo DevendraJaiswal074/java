f2d3c24c9432a254c27694ac805d58150d1c5e6b
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2025-03-05 01:18:07 +0530

    Java 75

---
+++ LeetCode/933. Number of Recent Calls.java
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/number-of-recent-calls/description/?envType=study-plan-v2&envId=leetcode-75
+
+
+
+// Method 1 to solve the problem
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+class RecentCounter {
+    List<Integer> list;
+    int i;
+
+    public RecentCounter() {
+        list = new ArrayList();
+        i =0;
+    }
+    
+    public int ping(int t) {
+        list.add(t);
+        while (t-3000 > list.get(i) ) {
+            i++;
+        }
+        return list.size()-i;
+    }
+}
+
+// Method second 
+
+class RecentCounter {
+
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList();
+    }
+    
+    public int ping(int t) {
+        queue.add(t);
+        while(!queue.isEmpty() && t - 3000 > queue.peek()){
+            queue.pop();
+        }
+        return queue.size;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
