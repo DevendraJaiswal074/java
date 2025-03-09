1d69f432790944a931fed807d2e98d69c8573481
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2025-03-09 23:12:41 +0530

    day 9 March

---
+++ LeetCode/3208. Alternating Groups II.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/alternating-groups-ii/description/?envType=daily-question&envId=2025-03-09
+
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int res=0;
+        int left=0;
+        int n = colors.length;
+        // N+K
+        for(int right=1;right < (n + k -1); right++){ // exp..
+            // skip entire subarray
+            if(colors[right%n] == colors[(right-1)%n]){
+                left = right;
+            }
+            if(right - left + 1 == k){
+                res++;
+                left++; // move to next subarray or shrinking phase
+            }
+        }
+        return res;
+    }
+}
