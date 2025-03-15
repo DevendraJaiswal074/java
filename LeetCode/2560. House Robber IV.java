2eeea40ea08791ede9291c62d25c5777a68afa4c
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2025-03-15 22:14:13 +0530

    Day 15(March)

---
+++ LeetCode/2560. House Robber IV.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/house-robber-iv/submissions/1574710289/?envType=daily-question&envId=2025-03-15
+
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        int start=Integer.MAX_VALUE;
+        int end=Integer.MIN_VALUE;
+        for(int num : nums){
+            if(num>end){
+                end = num;
+            }
+            if(num < start){
+                start = num;
+            }
+        }
+        int ans=0;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(isRobberyPossible(mid, nums, k)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    public boolean isRobberyPossible(int capability, int nums[], int minHouses){
+        int housesRobbed=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] <= capability){
+                housesRobbed++;
+                i++; //skip for adjacent condition
+            }
+            if(housesRobbed >= minHouses) return true;
+        }
+        return false;
+    }
+}
