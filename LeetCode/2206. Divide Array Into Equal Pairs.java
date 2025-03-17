f69309d220d0a9e8f24b4e4d15fd476029e07ac8
devendrajaiswal074 <devendrajaiswal074@gmail.com>
2025-03-17 23:34:39 +0530

    Day 17(March)

---
+++ LeetCode/2206. Divide Array Into Equal Pairs.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/divide-array-into-equal-pairs/description/?envType=daily-question&envId=2025-03-17
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                set.remove(num);
+            }else{
+                set.add(num);
+            }
+        }
+        return (set.size()==0);   
+    }
+}
