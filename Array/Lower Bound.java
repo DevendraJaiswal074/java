534e51bf53a991bc17ae4a11be90c78c577aa095
Devendra Jaiswal <devendrajaiswal074@gmail.com>
2025-06-24 23:59:52 +0530

    simple but important Q

---
+++ Array/Lower Bound.java
@@ -0,0 +1,24 @@
+package Array;
+
+//https://takeuforward.org/plus/dsa/problems/lower-bound-
+
+class Solution {
+    public int lowerBound(int[] nums, int x) {
+
+        int leftSide = 0;
+        int rightSide = nums.length;
+
+        while(leftSide < rightSide){
+            int mid = leftSide + (rightSide-leftSide)/2;
+
+            if(nums[mid]<x){
+                leftSide = mid+1;
+            }
+            else{
+                rightSide = mid;
+            }
+        }
+        return leftSide;
+     }
+}
+
