1c6539f60d42a6861459be3be8483ed1d2e6720b
Devendra Jaiswal <devendrajaiswal074@gmail.com>
2025-07-02 23:45:33 +0530

    proper code, imp

--- LeetCode/July2025/3333. Find the Original Typed String II.java
+++ LeetCode/July2025/3333. Find the Original Typed String II.java
@@ -3,33 +3,8 @@
 package LeetCode.July2025;
 
 import java.util.*;
-// import java.util.ArrayList;
-// import java.util.Arrays;
-// import java.util.List;
-
 class Solution {
     int M = (int)1e9 + 7;
-    int[][] t;
-
-    public int solve(int i, int count, List<Integer> freq, int k) {
-        if (i >= freq.size()) {
-            if (count < k) return 1; // found invalid string
-            return 0;
-        }
-
-        if (t[i][count] != -1) return t[i][count];
-
-        long result = 0;
-        for (int take = 1; take <= freq.get(i); take++) {
-            if (count + take < k) {
-                result = (result + solve(i + 1, count + take, freq, k)) % M;
-            } else {
-                break;
-            }
-        }
-
-        return t[i][count] = (int) result;
-    }
 
     public int possibleStringCount(String word, int k) {
         if (k > word.length()) return 0;
@@ -54,10 +29,33 @@ class Solution {
         if (freq.size() >= k) return (int) P;
 
         int n = freq.size();
-        t = new int[n + 1][k + 1];
-        for (int[] row : t) Arrays.fill(row, -1); // initialize memo table
+        int[][] t = new int[n + 1][k + 1];
 
-        long invalidCount = solve(0, 0, freq, k);
+        for (int c = k - 1; c >= 0; c--) {
+            t[n][c] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int[] prefix = new int[k + 2];
+            for (int h = 1; h <= k; h++) {
+                prefix[h] = (prefix[h - 1] + t[i + 1][h - 1]) % M;
+            }
+
+            for (int c = k - 1; c >= 0; c--) {
+                int l = c + 1;
+                int r = c + freq.get(i);
+
+                if (r + 1 > k) {
+                    r = k - 1;
+                }
+
+                if (l <= r) {
+                    t[i][c] = (prefix[r + 1] - prefix[l] + M) % M;
+                }
+            }
+        }
+
+        long invalidCount = t[0][0];
         return (int)((P - invalidCount + M) % M);
     }
 }
