07ad95cb70b4afd67d3913f29d2f978ef75a8347
Devendra Jaiswal <devendrajaiswal074@gmail.com>
2025-07-12 19:12:03 +0530

    easy level Matrix

---
+++ LeetCode/Practice/74. Search a 2D Matrix.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/search-a-2d-matrix/description/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length-1;
+
+        while(row < matrix.length && col >= 0){
+
+            if(matrix[row][col] == target){
+                return true;
+            }
+
+            else if(target < matrix[row][col]){
+                col--;
+            }
+
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+}
