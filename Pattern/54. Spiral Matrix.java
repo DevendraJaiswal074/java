79647da9e5c6cd7dcf6d14a01e02b713fd0ab178
Devendra Jaiswal <devendrajaiswal074@gmail.com>
2025-07-12 17:08:18 +0530

    new approach

--- Pattern/54. Spiral Matrix.java
+++ Pattern/54. Spiral Matrix.java
@@ -48,4 +48,52 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<>();
+
+        int startrow = 0;
+        int startcol = 0;
+        int endrow = matrix.length-1;
+        int endcol = matrix[0].length-1;
+
+        while(startrow <= endrow && startcol <= endcol){
+
+            //to print top of matrix
+            for(int i = startcol; i<= endcol; i++){
+                result.add(matrix[startrow][i]);
+            }
+
+            //right side 
+            for(int j = startrow; j<= endrow; j++){
+                result.add(matrix[j][endcol]);
+            }
+
+            //bottom 
+            for(int k = endcol-1; k >= startcol ; k--){
+                if(startrow == endrow){
+                    break;
+                }
+                result.add(matrix[endrow][k]);
+            }
+
+            //left
+            for(int k2 = endrow; k2 >= startrow; k2--){
+                if(startcol == endcol){
+                    break;
+                }
+                result.add(matrix[k2][endcol]);
+            }
+
+        }
+        startcol++;
+        startrow++;
+        endcol--;
+        endrow--;
+        return result;
+    }
+}
+    
 */
