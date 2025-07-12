3b3c104c7a7a005dd05326c3770bf6f26e5aa598
Devendra Jaiswal <devendrajaiswal074@gmail.com>
2025-07-12 18:04:26 +0530

    new solution

--- LeetCode/Practice/1572. Matrix Diagonal Sum.java
+++ LeetCode/Practice/1572. Matrix Diagonal Sum.java
@@ -18,3 +18,25 @@ class Solution {
         return sum;
     }
 }
+
+/*  2nd approach
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+
+        for(int i = 0; i < mat.length; i++){
+            //primary diagonal
+            sum += mat[i][i];
+
+            //secondary diagonal
+            if(i != mat.length-i-1){
+                sum += mat[i][mat.length-i-1];
+            }
+        }
+
+        return sum;
+    }
+}
+
+*/
