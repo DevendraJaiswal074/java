e644027b56e4b747b4335931971a8b66ed150dce
Devendra Jaiswal <devendrajaiswal074@gmail.com>
2025-12-15 23:58:34 +0530

    Leetcode

---
+++ String/13. Roman to Integer.java
@@ -0,0 +1,36 @@
+package String;
+
+// https://leetcode.com/problems/roman-to-integer/description/?envType=problem-list-v2&envId=string
+
+class Solution {
+    public int romanToInt(String s) {
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr = value(s.charAt(i));
+
+            // If next value is greater, subtract current
+            if (i + 1 < s.length() && curr < value(s.charAt(i + 1))) {
+                total -= curr;
+            } else {
+                total += curr;
+            }
+        }
+
+        return total;
+    }
+
+    private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
